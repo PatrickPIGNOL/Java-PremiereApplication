88b99fcce3054b504c381894a7783e0d5d8b7a60
Patrick PIGNOL <patrick.pignol@gmail.com>
2020-09-11 02:17:02 +0200

    Update .classpath, .gitignore, and 2 more files...

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main 
+{
+	public static void main(String[] pArguments)
+	{
+		for(int i = 1; i<10; i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println("Hello world !");
+	}
+}
